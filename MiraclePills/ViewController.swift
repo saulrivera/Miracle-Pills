5abd1953ca0f9dec5bf189060ad3c1ac65f046ab
Saúl Rivera <saulriveal@gmail.com>
2017-06-27 23:46:19 -0500

    Finishing UI until success image view

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var statePickerBtn: UIButton!
     
+    @IBOutlet weak var pillImage: UIImageView!
+    @IBOutlet weak var labelPill: UILabel!
+    @IBOutlet weak var pricePillLabel: UILabel!
+    
+    @IBOutlet weak var divider: UIView!
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var fullNameEntry: UITextField!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var addressEntry: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityEntry: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var entryCountry: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var entryZip: UITextField!
+    @IBOutlet weak var buyButton: UIButton!
+    
+    @IBOutlet weak var successImg: UIImageView!
+    
     let states = ["Durango", "Nuevo Leon", "Jalisco", "Youcatan", "Ciudad de Mexico", "Aguascalientes"]
     
     override func viewDidLoad() {
@@ -28,6 +49,37 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBAction func stateBottonPressed(_ sender: Any) {
         statePicker.isHidden = false
+        countryLabel.isHidden = true
+        entryCountry.isHidden = true
+        zipLabel.isHidden = true
+        entryZip.isHidden = true
+        buyButton.isHidden = true
+    }
+    
+    @IBAction func buyButtonPressed(_ sender: Any) {
+        
+        pillImage.isHidden = true
+        labelPill.isHidden = true
+        pricePillLabel.isHidden = true
+        
+        divider.isHidden = true
+        
+        fullNameLabel.isHidden = true
+        fullNameEntry.isHidden = true
+        addressLabel.isHidden = true
+        addressEntry.isHidden = true
+        cityLabel.isHidden = true
+        cityEntry.isHidden = true
+        stateLabel.isHidden = true
+        statePicker.isHidden = true
+        statePickerBtn.isHidden = true
+        countryLabel.isHidden = true
+        entryCountry.isHidden = true
+        zipLabel.isHidden = true
+        entryZip.isHidden = true
+        buyButton.isHidden = true
+        
+        successImg.isHidden = false
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -45,6 +97,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         statePickerBtn.setTitle(states[row], for: UIControlState.normal)
         statePicker.isHidden = true
+        countryLabel.isHidden = false
+        entryCountry.isHidden = false
+        zipLabel.isHidden = false
+        entryZip.isHidden = false
+        buyButton.isHidden = false
     }
     
 }
